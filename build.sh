76f59b188b6bc1b41df848861d469d3b66f5b7a5
Spandy <freetousebmsit@gmail.com>
2024-11-23 00:38:39 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 set -o errexit
 
-pip insall -r requirements.txt
+pip install -r requirements.txt
 
-python manae.py collectstatic --no-input
+python manage.py collectstatic --no-input
 python manage.py migrate
